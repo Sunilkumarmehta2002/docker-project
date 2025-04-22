9a41162107c379e019d2027759804c2d63c027f2
Sunil kumar mehta <115181350+Sunilkumarmehta2002@users.noreply.github.com>
2025-04-22 13:47:49 +0530

    Update OurTeams.jsx

--- client/src/pages/OurTeams.jsx
+++ client/src/pages/OurTeams.jsx
@@ -23,7 +23,7 @@ const OurTeam = () => {
 
           <TeamMember
             imgSrc="https://i.ibb.co/YRwv4Z9/SUNIL.png"
-            name="Sunil Mehta"
+            name="Sunil Kumar Mehta"
             role="Founder & Developer"
             description="Born developer & technical strategy of the  platform and brand "
           />
